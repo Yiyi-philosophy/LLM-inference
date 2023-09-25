5cd5338f78632648019f2827e89110845842f64e
Yiyi-philosophy <yiyidematthew6@outlook.com>
2023-09-25 00:46:26 -0700

    A6000

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #conda activate flexgen
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=0
 #export CUDA_VISIBLE_DEVICES=0
-set PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32
+# set PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32
 python -m ipdb flexgen/flex_opt.py --model facebook/opt-1.3b --gpu-batch-size 8 --percent 100 0 100 0 100 0 --cut-gen-len 8 --num-gpu-batches 8
