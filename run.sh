4e87373fd60602b1d9c4a56d5305c6fa79cd7283
Yiyi-philosophy <yiyidematthew6@outlook.com>
2023-09-25 00:48:27 -0700

    A6000-2

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 export CUDA_VISIBLE_DEVICES=0
 #export CUDA_VISIBLE_DEVICES=0
 # set PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32
+
 python -m ipdb flexgen/flex_opt.py --model facebook/opt-1.3b --gpu-batch-size 8 --percent 100 0 100 0 100 0 --cut-gen-len 8 --num-gpu-batches 8
